b61a3a5f217a9a4f0517dbfe8f4ce830304278f6
nicky-mc <cinimod@gmail.com>
2024-10-07 11:29:26 +0100

    folder set up

---
+++ React-route/src/App.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h1>Welcome to my website</h1>
+      </div>
+    </BrowserRouter>
+  );
+}
